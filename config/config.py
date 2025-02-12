cb56da4977f75fb226cad80fd289876b833defa2
Artur Ruzilevich <bigflyfly33@gmail.com>
2025-02-12 09:49:36 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,17 +1,4 @@
-# import os
-# from dotenv import load_dotenv
 
-# load_dotenv()
-
-# TELEGRAM_BOT_TOKEN = "7592753727:AAGI3aVDF4angQr5Nn9TMRNpwonBY7EWEtE"
-# GOOGLE_SHEET_ID = "17Vkp9p_FLihy7IESWUs6eIvJVOoXGgUL"
-# GOOGLE_SHEET_CONTACTS_ID = "16OWjP2rT8ObeGVibKX-a4jtRD4U_DkMU2Pllus9VRGc"
-# GOOGLE_PROJECT_ID = "teleiagent"
-# GOOGLE_PRIVATE_KEY_ID = "d525f29db58ca8cdfa15e324d0df7580814ea651"
-# GOOGLE_PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCz..."
-# GOOGLE_CLIENT_EMAIL = "sheetsbot@teleiagent.iam.gserviceaccount.com"
-# GOOGLE_CLIENT_ID = "110530763400503171328"
-# GOOGLE_CLIENT_X509_CERT_URL = "https://www.googleapis.com/robot/v1/metadata/x509/sheetsbot%40teleiagent.iam.gserviceaccount.com"
 
 import os
 from dotenv import load_dotenv
